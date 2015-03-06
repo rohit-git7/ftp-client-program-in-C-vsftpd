4f49454dbaf0d622d39720630a2a3910062068a2
Rohit Takhar <rohit.takhar1994@gmail.com>
2015-03-07 00:30:34 +0530

    Update ftp_client.c

--- ftp_client.c
+++ ftp_client.c
@@ -7,10 +7,161 @@
 #include<string.h>
 #include<ctype.h>//isdigit()
 #include<fcntl.h>//open()
+#include<dirent.h>
 #include<sys/stat.h>//stat()
+#include<grp.h>
+#include<pwd.h>
+#include<time.h>
 
 #define PORT 21
 #define MAXSZ 1024
+
+void ls_dir(char *dir_name)
+{
+	DIR *fd;
+	
+	struct dirent *entry;
+	fd = opendir(dir_name);
+	if(fd == NULL)
+	{
+		perror("Error");
+		return;
+	}
+
+	while((entry = readdir(fd))!=NULL)
+	{
+	    if(entry->d_name[0] != '.' && strcmp(entry->d_name,".") != 0 && strcmp(entry->d_name,"..") != 0)
+		printf("%s\n",entry->d_name);
+	}
+	
+	printf("\n");
+	closedir(fd);
+}
+
+void ls_l_dir(char *dir_name)
+{
+	char time_buff[MAXSZ];
+
+	int val = 0;
+	int temp;
+
+	struct group *gp;
+	struct passwd *pw;
+	struct tm *info;
+	struct stat buff;
+	struct dirent *entry;
+	DIR *fd;
+	fd  = opendir(dir_name);
+	if(fd == NULL)
+	{
+		perror("Error");
+		return;
+	}
+	
+	while((entry = readdir(fd))!= NULL)
+	{
+		
+		lstat(entry->d_name,&buff);
+	   	gp = getgrgid(buff.st_gid);		
+		pw = getpwuid(buff.st_uid);
+		info = localtime(&(buff.st_mtime));
+		strftime(time_buff,sizeof(time_buff),"%b %d %H:%M",info);
+	   if(strcmp(entry->d_name,".") != 0 && strcmp(entry->d_name,"..") != 0 && entry->d_name[0]!= '.')
+ 		{
+			if(buff.st_size % 1024 == 0)
+			{
+				temp = (int)buff.st_size / 1024;
+				val += temp ;
+			}
+			else
+			{
+				temp = ((int)buff.st_size / 1024);
+				val += (temp + (4 - (temp % 4)));
+			}
+
+
+			switch(buff.st_mode & S_IFMT)
+			{
+				case S_IFCHR:
+					printf("c");
+					break;
+				case S_IFBLK:
+					printf("b");
+					break;
+				case S_IFDIR:
+                        		printf("d");
+					break;
+				case S_IFLNK:
+					printf("l");
+					break;
+				case S_IFIFO:
+					printf("p");
+					break;
+				case S_IFSOCK:
+					printf("s");
+					break;
+				default:
+					printf("-");
+					break;
+			}
+
+		if(buff.st_mode & S_IRUSR)
+			printf("r");
+		else
+			printf("-");
+
+		if(buff.st_mode & S_IWUSR)
+			printf("w");
+		else
+			printf("-");
+
+		if(buff.st_mode & S_IXUSR)
+			printf("x");
+		else
+			printf("-");
+
+		if(buff.st_mode & S_IRGRP)
+			printf("r");
+		else
+			printf("-");
+
+		if(buff.st_mode & S_IWGRP)
+			printf("w");
+		else
+			printf("-");
+
+		if(buff.st_mode & S_IXGRP)
+			printf("x");
+		else
+			printf("-");
+
+		if(buff.st_mode & S_IROTH)
+			printf("r");
+		else
+			printf("-");
+
+		if(buff.st_mode & S_IWOTH)
+			printf("w");
+		else
+			printf("-");
+			
+		if(buff.st_mode & S_IXOTH)
+			printf("x");
+		else
+			printf("-");
+
+
+		if(((buff.st_mode & S_IFMT)^S_IFCHR) == 0 || ((buff.st_mode & S_IFMT)^S_IFBLK) ==0)
+			printf(" %6d %8s %8s %5d, %5d %13s %s\n",(int)buff.st_nlink,pw->pw_name,gp->gr_name,major(buff.st_rdev),minor(buff.st_rdev),time_buff,entry->d_name);
+		else
+			printf(" %6d %8s %8s %12u %13s %s\n",(int)buff.st_nlink,pw->pw_name,gp->gr_name,(unsigned int)buff.st_size,time_buff,entry->d_name);
+		
+		}
+	}
+	printf("total %d\n\n",val);
+	closedir(fd);
+
+}
 
 int passive_port_number(char *message)
 {
@@ -95,10 +246,10 @@ int main(int argc, char *argv[])
 	char data[4096];//send and get data
 	char file_name[MAXSZ];//file name to be sent to server
 	char file[MAXSZ];//file to be created on client
-	char username[MAXSZ];//user name entered by the user
+	char username[MAXSZ];//username entered by the user
+	char working_dir[MAXSZ];
 
-
-	char *password=malloc(MAXSZ);//password entered by user
+	char *password=malloc(MAXSZ);//password enterd by user
 	char passive[]="PASV\r\n";	
 
 	sockfd = socket(AF_INET,SOCK_STREAM,0);
@@ -157,6 +308,7 @@ int main(int argc, char *argv[])
 		bzero(user_input,MAXSZ);
 		bzero(message_to_server,MAXSZ);
 		bzero(message_from_server,MAXSZ);
+		bzero(working_dir,MAXSZ);
 		bzero(data,4096);
 	
 		no_of_bytes = read(0,user_input,MAXSZ);
@@ -176,6 +328,37 @@ int main(int argc, char *argv[])
 			message_from_server[no_of_bytes] = '\0';
 			printf("%s\n",message_from_server);
 			break;
+		}
+
+		if(memcmp(user_input,"!cd ",4) == 0 || strcmp(user_input,"!cd") == 0)
+		{
+			if(chdir(user_input + 4) == 0)
+			{
+				printf("Directory successfully changed\n\n");
+			}			
+			else
+			{
+				perror("Error");
+			}
+		}
+
+		if(memcmp(user_input,"!pwd ",5) == 0 || strcmp(user_input,"!pwd") == 0)
+		{
+			getcwd(working_dir,MAXSZ);	
+			printf("%s\n\n",working_dir);
+		}	
+
+		if(memcmp(user_input,"!ls -l ",7) == 0 || strcmp(user_input,"!ls -l") == 0)
+		{
+			getcwd(working_dir,MAXSZ);	
+			ls_l_dir(working_dir);
+			continue;
+		}
+			
+		if(memcmp(user_input,"!ls ",4) == 0 || strcmp(user_input,"!ls") == 0)
+		{
+			getcwd(working_dir,MAXSZ);	
+			ls_dir(working_dir);
 		}
 		
 		if(memcmp(user_input,"cd ",3) == 0)
