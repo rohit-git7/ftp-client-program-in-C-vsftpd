d17b86791678c054288ad216c9529a3fa3cc669a
Rohit Takhar <rohit.takhar1994@gmail.com>
2015-03-14 23:29:32 +0530

    upload files to server

--- put_content.h
+++ put_content.h
@@ -1,6 +1,9 @@
-
+/*
+Upload files on server.
+*/
 void put_content(char *arg,char *user_input,int sockfd)
 {	
+	/* Temporary variables*/
 	int no_of_bytes;
 	int port;	
 	int newsockfd;
@@ -13,40 +16,51 @@ void put_content(char *arg,char *user_input,int sockfd)
 
 	char message_from_server[MAXSZ];
 	char message_to_server[MAXSZ];
-	char file[MAXSZ];
-	char file_name[MAXSZ];
-	char data[4096];
+	char file[MAXSZ];// File name
+	char file_name[MAXSZ];// File name with instruction to server
+	char data[MAXSZ];// Data transfer
 
+	/* Initialise all the character arrays */
 	bzero(message_from_server,MAXSZ);
 	bzero(message_to_server,MAXSZ);
 	bzero(file_name,MAXSZ);
 	bzero(file,MAXSZ);
-	bzero(data,4096);
+	bzero(data,MAXSZ);
 	
+	/* Tell server to change to BINARY mode */
 	send(sockfd,"TYPE I\r\n",8,0);
 	no_of_bytes = recv(sockfd,message_from_server,MAXSZ,0);
 	message_from_server[no_of_bytes] = '\0';
 	printf("%s\n",message_from_server);
-			
+	fflush(stdout);	
+	
+	/* Send request for PASSIVE connection */	
 	send(sockfd,passive,strlen(passive),0);
 	no_of_bytes = recv(sockfd,message_from_server,MAXSZ,0);
 	message_from_server[no_of_bytes] = '\0';
 	printf("%s\n",message_from_server);
-		
-	if(memcmp(message_from_server,"227",3)== 0)
+	fflush(stdout);	
+	
+	/* Server accepts request and sends PORT variables */
+	if(strncmp(message_from_server,"227",3)== 0)
 	{
+		/* Generate a PORT number using PORT variables */
 		port = passive_port_number(message_from_server); 
 	
+		/* Connect to server using another PORT for file transfers */
 		newsockfd = func_to_connect_passive(arg,port);
-			
+		
+		/* Send file name to server */
 		sprintf(file_name,"STOR %s\r\n",user_input + 4);	
 		send(sockfd,file_name,strlen(file_name),0);
 			
 		no_of_bytes = recv(sockfd,message_from_server,MAXSZ,0);
 		message_from_server[no_of_bytes] = '\0';
 		printf("%s\n",message_from_server);
-				
-		if(memcmp(message_from_server,"150",3)== 0 || memcmp(message_from_server,"125",3)== 0)
+		fflush(stdout);	
+		
+		/* Send file data to server */
+		if(strncmp(message_from_server,"150",3)== 0 || strncmp(message_from_server,"125",3)== 0)
 		{
 			sprintf(file,"%s",user_input + 4);
 
@@ -55,7 +69,7 @@ void put_content(char *arg,char *user_input,int sockfd)
 			size = (int)buff.st_size;
 			while(size > 0)
 			{
-				no_of_bytes = read(fd,data,4096);
+				no_of_bytes = read(fd,data,MAXSZ);
 				total = 0;
 				while(total < no_of_bytes)
                 		{
@@ -69,7 +83,7 @@ void put_content(char *arg,char *user_input,int sockfd)
 			no_of_bytes = recv(sockfd,message_from_server,MAXSZ,0);
 			message_from_server[no_of_bytes] = '\0';
 			printf("%s\n",message_from_server);
-					
+			fflush(stdout);			
 		}
 				
 	}
